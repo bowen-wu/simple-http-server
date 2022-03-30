947a1809d7a761dbbe41a24b4530ffc5a169ddbb
bowen-wu <13793248162@163.com>
2022-03-30 17:09:37 +0800

    feat: optimize

--- src/main/java/com/bowen/http/SimpleHttpServer.java
+++ src/main/java/com/bowen/http/SimpleHttpServer.java
@@ -2,19 +2,15 @@ package com.bowen.http;
 
 import javax.annotation.Resources;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Path;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,46 +29,23 @@ public class SimpleHttpServer {
             Socket socket = serverSocket.accept();
             System.out.println(socket);
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            List<String> messageList = new ArrayList<>();
-            String str;
-            while (!(str = bufferedReader.readLine()).isEmpty()) {
-                System.out.println(str);
-                messageList.add(str);
-            }
+            // 获取报文信息
+            List<String> messageList = getMessageList(socket);
 
             String resourceName = getMatcherStr(messageList.get(0));
 
             // HTTP response header
             socket.getOutputStream().write("HTTP/1.1 200 OK\r\n".getBytes());
 
-            if (resourceName.endsWith(".js") || resourceName.endsWith(".css") || resourceName.endsWith(".jpeg")) {
-                if (resourceName.endsWith(".js")) {
-                    socket.getOutputStream().write("content-type: text/javascript\r\n".getBytes());
-                } else if (resourceName.endsWith(".css")) {
-                    socket.getOutputStream().write("content-type: text/css\r\n".getBytes());
-                } else if (resourceName.endsWith(".jpeg")) {
-                    socket.getOutputStream().write("content-type: image/jpeg\r\n".getBytes());
-                }
+            if (isFile(resourceName, ".js") || isFile(resourceName, ".css") || isFile(resourceName, ".jpeg")) {
+                setHeader(socket, resourceName);
 
                 // HTTP response header/body 分割符
                 socket.getOutputStream().write("\r\n".getBytes());
 
                 InputStream inputStream = Resources.class.getResourceAsStream(resourceName);
                 assert inputStream != null;
-                if (resourceName.endsWith(".js") || resourceName.endsWith(".css")) {
-                    BufferedReader file = new BufferedReader(new InputStreamReader(inputStream));
-                    String fileLine;
-                    while ((fileLine = file.readLine()) != null) {
-                        socket.getOutputStream().write(fileLine.getBytes());
-                    }
-                } else if (resourceName.endsWith(".jpeg")) {
-                    int b;
-                    while ((b = inputStream.read()) != -1) {
-                        socket.getOutputStream().write(b);
-                    }
-                }
+                writeOutputStream(socket, resourceName, inputStream);
 
                 // submit
                 socket.getOutputStream().flush();
@@ -92,6 +65,47 @@ public class SimpleHttpServer {
             }
 
             socket.close();
+        }
+    }
+
+    private static void writeOutputStream(Socket socket, String resourceName, InputStream inputStream) throws IOException {
+        if (isFile(resourceName, ".js") || isFile(resourceName, ".css")) {
+            BufferedReader file = new BufferedReader(new InputStreamReader(inputStream));
+            String fileLine;
+            while ((fileLine = file.readLine()) != null) {
+                socket.getOutputStream().write(fileLine.getBytes());
+            }
+        } else if (isFile(resourceName, ".jpeg")) {
+            int b;
+            while ((b = inputStream.read()) != -1) {
+                socket.getOutputStream().write(b);
+            }
+        }
+    }
+
+    private static boolean isFile(String resourceName, String suffix) {
+        return resourceName.endsWith(suffix);
+    }
+
+    private static List<String> getMessageList(Socket socket) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        List<String> messageList = new ArrayList<>();
+        String str;
+        while (!(str = bufferedReader.readLine()).isEmpty()) {
+            System.out.println(str);
+            messageList.add(str);
+        }
+        return messageList;
+    }
+
+    private static void setHeader(Socket socket, String resourceName) throws IOException {
+        if (isFile(resourceName, ".js")) {
+            socket.getOutputStream().write("content-type: text/javascript\r\n".getBytes());
+        } else if (isFile(resourceName, ".css")) {
+            socket.getOutputStream().write("content-type: text/css\r\n".getBytes());
+        } else if (isFile(resourceName, ".jpeg")) {
+            socket.getOutputStream().write("content-type: image/jpeg\r\n".getBytes());
         }
     }
 
