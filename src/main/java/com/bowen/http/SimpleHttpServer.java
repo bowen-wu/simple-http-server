ff659322ed6c361fa08642d1b452d2c5e9ae46ec
bowen-wu <13793248162@163.com>
2022-03-30 17:03:06 +0800

    feat: server return image file

--- src/main/java/com/bowen/http/SimpleHttpServer.java
+++ src/main/java/com/bowen/http/SimpleHttpServer.java
@@ -2,11 +2,15 @@ package com.bowen.http;
 
 import javax.annotation.Resources;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Path;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,20 +42,36 @@ public class SimpleHttpServer {
                 messageList.add(str);
             }
 
-            String resource = getMatcherStr(messageList.get(0));
+            String resourceName = getMatcherStr(messageList.get(0));
 
             // HTTP response header
             socket.getOutputStream().write("HTTP/1.1 200 OK\r\n".getBytes());
-            if (resource.endsWith(".js") || resource.endsWith(".css")) {
-                socket.getOutputStream().write("content-type: text/javascript\r\n".getBytes());
+
+            if (resourceName.endsWith(".js") || resourceName.endsWith(".css") || resourceName.endsWith(".jpeg")) {
+                if (resourceName.endsWith(".js")) {
+                    socket.getOutputStream().write("content-type: text/javascript\r\n".getBytes());
+                } else if (resourceName.endsWith(".css")) {
+                    socket.getOutputStream().write("content-type: text/css\r\n".getBytes());
+                } else if (resourceName.endsWith(".jpeg")) {
+                    socket.getOutputStream().write("content-type: image/jpeg\r\n".getBytes());
+                }
 
                 // HTTP response header/body 分割符
                 socket.getOutputStream().write("\r\n".getBytes());
 
-                BufferedReader file = new BufferedReader(new InputStreamReader(Objects.requireNonNull(Resources.class.getResourceAsStream(resource))));
-                String fileLine;
-                while ((fileLine = file.readLine()) != null) {
-                    socket.getOutputStream().write(fileLine.getBytes());
+                InputStream inputStream = Resources.class.getResourceAsStream(resourceName);
+                assert inputStream != null;
+                if (resourceName.endsWith(".js") || resourceName.endsWith(".css")) {
+                    BufferedReader file = new BufferedReader(new InputStreamReader(inputStream));
+                    String fileLine;
+                    while ((fileLine = file.readLine()) != null) {
+                        socket.getOutputStream().write(fileLine.getBytes());
+                    }
+                } else if (resourceName.endsWith(".jpeg")) {
+                    int b;
+                    while ((b = inputStream.read()) != -1) {
+                        socket.getOutputStream().write(b);
+                    }
                 }
 
                 // submit
@@ -65,15 +85,7 @@ public class SimpleHttpServer {
                 socket.getOutputStream().write("\r\n".getBytes());
 
                 // HTTP response body
-                socket.getOutputStream().write(("<html>" +
-                        "<script src='/myScript.js'></script>" +
-                        "<link href='style.css' rel='stylesheet'>" +
-                        "<h1>Hello</h1>" +
-                        "<p>World!</p>" +
-                        "<div>Text</div>" +
-                        "<div class='text'>Text with class</div>" +
-                        Instant.now() +
-                        "</html>").getBytes());
+                socket.getOutputStream().write(("<html>" + "<script src='/myScript.js'></script>" + "<link href='style.css' rel='stylesheet'>" + "<h1>Hello</h1>" + "<p>World!</p>" + "<div>Text</div>" + "<img src='image.jpeg'/>" + "<div class='text'>Text with class</div>" + Instant.now() + "</html>").getBytes());
 
                 // submit
                 socket.getOutputStream().flush();
