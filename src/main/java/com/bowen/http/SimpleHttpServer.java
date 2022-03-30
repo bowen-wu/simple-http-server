3bb8a05323c4fcc761297472db509ca354a06d67
bowen-wu <13793248162@163.com>
2022-03-30 16:28:10 +0800

    feat: server return js file

--- src/main/java/com/bowen/http/SimpleHttpServer.java
+++ src/main/java/com/bowen/http/SimpleHttpServer.java
@@ -1,5 +1,6 @@
 package com.bowen.http;
 
+import javax.annotation.Resources;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,8 +8,15 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SimpleHttpServer {
+    public static Pattern RESOURCE_PATTERN = Pattern.compile("/.*\\s");
+
     public static void main(String[] args) throws IOException {
         int port = 8080;
         ServerSocket serverSocket = new ServerSocket();
@@ -16,38 +24,67 @@ public class SimpleHttpServer {
         // 绑定 port
         serverSocket.bind(new InetSocketAddress("127.0.0.1", port));
 
-        // Listener 阻塞
-        Socket socket = serverSocket.accept();
-        System.out.println(socket);
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        StringBuilder stringBuilder = new StringBuilder();
         while (true) {
-            String str = bufferedReader.readLine();
-            if (str == null || str.isEmpty()) {
-                break;
+            // Listener 阻塞
+            Socket socket = serverSocket.accept();
+            System.out.println(socket);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            List<String> messageList = new ArrayList<>();
+            String str;
+            while (!(str = bufferedReader.readLine()).isEmpty()) {
+                System.out.println(str);
+                messageList.add(str);
             }
 
-            stringBuilder.append(str);
-            stringBuilder.append("\r\n");
-        }
+            String resource = getMatcherStr(messageList.get(0));
 
-        System.out.println(stringBuilder);
+            // HTTP response header
+            socket.getOutputStream().write("HTTP/1.1 200 OK\r\n".getBytes());
+            if (resource.endsWith(".js")) {
+                socket.getOutputStream().write("content-type: text/javascript\r\n".getBytes());
 
-        // HTTP response header
-        socket.getOutputStream().write("HTTP/1.1 200 OK\r\n".getBytes());
-        socket.getOutputStream().write("content-type: text/html\r\n".getBytes());
+                // HTTP response header/body 分割符
+                socket.getOutputStream().write("\r\n".getBytes());
+
+                BufferedReader file = new BufferedReader(new InputStreamReader(Objects.requireNonNull(Resources.class.getResourceAsStream(resource))));
+                String fileLine;
+                while ((fileLine = file.readLine()) != null) {
+                    socket.getOutputStream().write(fileLine.getBytes());
+                }
+
+                // submit
+                socket.getOutputStream().flush();
+            } else {
+                socket.getOutputStream().write("content-type: text/html\r\n".getBytes());
 //        socket.getOutputStream().write("content-type: text/plain\r\n".getBytes());
 //        socket.getOutputStream().write("content-disposition: attachment;filename=123.txt\r\n".getBytes());
 
-        // HTTP response header/body 分割符
-        socket.getOutputStream().write("\r\n".getBytes());
+                // HTTP response header/body 分割符
+                socket.getOutputStream().write("\r\n".getBytes());
 
-        // HTTP response body
-        socket.getOutputStream().write(("<html><h1>Hello</h1><p>World!</p>" + Instant.now() + "</html>").getBytes());
+                // HTTP response body
+                socket.getOutputStream().write(("<html>" +
+                        "<script src='/myScript.js'></script>" +
+                        "<h1>Hello</h1><p>World!</p>" +
+                        Instant.now() +
+                        "</html>").getBytes());
 
-        // submit
-        socket.getOutputStream().flush();
+                // submit
+                socket.getOutputStream().flush();
+            }
+
+            socket.close();
+        }
+    }
+
+    public static String getMatcherStr(String target) {
+        Matcher matcher = RESOURCE_PATTERN.matcher(target);
+        while (matcher.find()) {
+            String str = matcher.group(0);
+            return str.substring(0, str.length() - 1);
+        }
+        return "";
     }
 }
